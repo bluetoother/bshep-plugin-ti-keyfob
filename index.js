68a81d3eb912498ee5488ef1c88ab3c97808bc24
Hedy Wang <hedywings@gmail.com>
2016-07-26 10:39:11 +0800

    motify

--- index.js
+++ index.js
@@ -10,19 +10,24 @@ module.exports = {
             {name: 'acceleroX', uuid: '0xffa3', params: ['x'], types: ['uint8']}, 
             {name: 'acceleroY', uuid: '0xffa4', params: ['y'], types: ['uint8']}, 
             {name: 'acceleroZ', uuid: '0xffa5', params: ['z'], types: ['uint8']}, 
-            {name: 'keyPressState', uuid: '0xffe1', params: ['enable'], types: ['uint8']}, 
+            {name: 'keyPressState', uuid: '0xffe1', params: ['enable'], types: ['uint8']}
         ]
     },
-    analysis: function (periph, basicInfo) {
-        var checkFlag = false,
-            servList = _.keys(periph.servs);
+    examine: function (periph, basicInfo) {
+        var isMine  = false,
+            checkServList = [ '0x1802', '0x1803', '0x1804', '0x180f', '0xffa0', '0xffe0' ],
+            count = 0;
 
-        if (basicInfo.devName === 'TI BLE Keyfob' && _.includes(servList, '0x1802') && 
-            _.includes(servList, '0x1803') && _.includes(servList, '0x1804') && 
-            _.includes(servList, '0x180f') && _.includes(servList, '0xffa0') &&
-            _.includes(servList, '0xffe0'))
-            checkFlag = true;
+        if (basicInfo.devName === 'TI BLE Keyfob' ) {
+            for(var uuid in periph.servs) {
+                for(var i = 0; i < checkServList.length; i += 1)
+                    if (uuid === checkServList[i]) count += 1;
+            }
 
-        return checkFlag;
+            if (count === 6)
+                isMine  = true;
+        }
+
+        return isMine;
     }
 };
